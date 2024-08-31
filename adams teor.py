3744885aa92aa640cb9d02c8e2c63092cca27baa
Verahanna <125619447+Verahanna@users.noreply.github.com>
2024-08-31 13:41:31 +0200

    Add files via upload

---
+++ adams teor.py
@@ -0,0 +1,14 @@
+inp1 = int
+inp2 = int
+out1 = int
+out2 = int
+
+print(input("inp1:".format(inp1) ))
+print(input("inp2:".format(inp2) ))
+print(input("out1:".format(out1) ))
+print(input("out2:".format(out2) ))
+
+if inp1 == out1 and inp2 == out2:
+    print("spravodlivost")
+else:
+    print("nespravodlivost")
